54e7d188553da35b5045dc0ed6f98ced3012a3f5
Pasqua <chideraemeka2@gmail.com>
2022-05-29 10:56:50 +0200

    feature: get one saved dish endpoint created

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,97 +16,90 @@ userController.upload = upload.array("files", 3);
 
 //user saves dish
 userController.saveDish = catchAsync(async (req, res, next) => {
-    /// dish Id is TTV-1 not 6281613d3159425cee8f8966
-    const dishId = req.params.dishId;
+  /// dish Id is TTV-1 not 6281613d3159425cee8f8966
+  const dishId = req.params.dishId;
 
-    // check if dish exist
-    let dish = await Dish.findOne({ dishId });
-    if (!dish) return next(new AppError("Dish not found", 404));
-  
-    //check if user is logged
-    let user = await User.findById({ _id: req.USER_ID });
-    if (!user) return next(new AppError("User not logged in", 400));
-  
-//1. check if schema with user already exists
+  // check if dish exist
+  let dish = await Dish.findOne({ dishId });
+  if (!dish) return next(new AppError("Dish not found", 404));
 
-const userSchemaExists = await SavedRecipe.exists({ user:user
-});
-if (userSchemaExists){
-  const existingUserSChema = await SavedRecipe.findOne({user: req.USER_ID})
- existingUserSChema.pushDish(dish)
-  dish.calcSavedDishes();
+  //check if user is logged
+  let user = await User.findById({ _id: req.USER_ID });
+  if (!user) return next(new AppError("User not logged in", 400));
 
-  dish.save((err, result) => {
-    if (err) return next(new AppError({ message: err.message }, 400));
-  });
+  //1. check if schema with user already exists
 
-  user.save((err, result) => {
-   if (err) return next(new AppError({ message: err.message }, 400));
- });
+  const userSchemaExists = await SavedRecipe.exists({ user: user });
+  if (userSchemaExists) {
+    const existingUserSChema = await SavedRecipe.findOne({ user: req.USER_ID });
+    existingUserSChema.pushDish(dish);
+    dish.calcSavedDishes();
 
- existingUserSChema.save((err, result) => {
-  if (err) return next(new AppError({ message: err.message }, 400));
-  else {
-    //send a response
-    res.status(201).send({
-      status: "success",
-      message: "Dish saved successfully",
+    dish.save((err, result) => {
+      if (err) return next(new AppError({ message: err.message }, 400));
+    });
+
+    user.save((err, result) => {
+      if (err) return next(new AppError({ message: err.message }, 400));
+    });
+
+    existingUserSChema.save((err, result) => {
+      if (err) return next(new AppError({ message: err.message }, 400));
+      else {
+        //send a response
+        res.status(201).send({
+          status: "success",
+          message: "Dish saved successfully",
+        });
+      }
+    });
+  } else {
+    //create id for frontend use
+    let savedDishId;
+    const lastSavedDish = await SavedRecipe.find().sort({ _id: -1 }).limit(1);
+    if (lastSavedDish.length == 0) {
+      savedDishId = "SD-1";
+    } else {
+      savedDishId =
+        "SD-" + `${Number(lastSavedDish[0].savedDishId.split("-")[1]) + 1}`;
+    }
+
+    //create new Schema --> essence of this is for the tik tok owner to know how many people saved/liked and want t try out their recipe -- to be reviewed
+    const newSavedDish = new SavedRecipe({
+      savedDishId: savedDishId,
+      user: user,
+    });
+
+    user.saveDish(newSavedDish._id);
+    user.save((err, result) => {
+      if (err) return next(new AppError({ message: err.message }, 400));
+    });
+
+    //instance method
+    newSavedDish.pushDish(dish);
+    dish.calcSavedDishes();
+
+    dish.save((err, result) => {
+      if (err) return next(new AppError({ message: err.message }, 400));
+    });
+
+    // ! populate ---> so we dont just give the id but also the full information
+    User.findById({ _id: req.USER_ID }).populate("savedDishes");
+    SavedRecipe.findOne({ savedDishId }).populate("savedDishes");
+    SavedRecipe.findOne({ savedDishId }).populate("user");
+
+    ///save the saved recipe schema
+    newSavedDish.save((err, result) => {
+      if (err) return next(new AppError({ message: err.message }, 400));
+      else {
+        //send a response
+        res.status(201).send({
+          status: "success",
+          message: "Dish saved successfully",
+        });
+      }
     });
   }
-});
-
-}else{
-  
-  //create id for frontend use
-  let savedDishId;
-  const lastSavedDish = await SavedRecipe.find().sort({ _id: -1 }).limit(1);
-  if (lastSavedDish.length == 0) {
-    savedDishId = "SD-1";
-  } else {
-    savedDishId =
-      "SD-" + `${Number(lastSavedDish[0].savedDishId.split("-")[1]) + 1}`;
-  }
-
-  //create new Schema --> essence of this is for the tik tok owner to know how many people saved/liked and want t try out their recipe -- to be reviewed
-  const newSavedDish = new SavedRecipe({
-    savedDishId: savedDishId,
-    user: user,
-  });
-
-
- user.saveDish(newSavedDish._id)
- user.save((err, result) => {
-  if (err) return next(new AppError({ message: err.message }, 400));
-});
-
-  //instance method
-  newSavedDish.pushDish(dish);
-  dish.calcSavedDishes();
-
-  dish.save((err, result) => {
-    if (err) return next(new AppError({ message: err.message }, 400));
-  });
-
-  // ! populate ---> so we dont just give the id but also the full information
-  User.findById({ _id: req.USER_ID }).populate("savedDishes")
-  SavedRecipe.findOne({ savedDishId }).populate("savedDishes");
-  SavedRecipe.findOne({ savedDishId }).populate("user");
-
-  ///save the saved recipe schema
-  newSavedDish.save((err, result) => {
-    if (err) return next(new AppError({ message: err.message }, 400));
-    else {
-      //send a response
-      res.status(201).send({
-        status: "success",
-        message: "Dish saved successfully",
-      });
-    }
-  });
-}
-
-
-
 });
 
 //get all user saved dishes
@@ -115,24 +108,44 @@ userController.getMySavedDishes = catchAsync(async (req, res, next) => {
   let user = await User.findById({ _id: req.USER_ID });
   if (!user) return next(new AppError("User not logged in", 400));
 
-  
-
   //get saved dishes of user X
-  const savedDishes = await SavedRecipe.find({user:req.USER_ID});
+  const savedDishes = await SavedRecipe.find({ user: req.USER_ID });
   if (!savedDishes) {
     return next(new AppError(`Could not GET all YOUR Saved Dishes`, 404));
   }
 
-console.log(savedDishes)
+  console.log(savedDishes);
 
   //send a response
   res.status(200).send({
     status: "status",
-    data: savedDishes ,
+    data: savedDishes,
   });
 });
 
-userController.getOneSavedDish = catchAsync(async (req, res, next) => {});
+userController.getOneSavedDish = catchAsync(async (req, res, next) => {
+  /// saved dish Id is TTV-1 not  SD-1
+  const dishId = req.params.dishId;
+
+  let dish = await Dish.findOne({ dishId });
+  if (!dish) return next(new AppError("Dish not found", 404));
+
+  const regUser = await SavedRecipe.findOne({ user: req.USER_ID });
+  if (!regUser)
+    return next(new AppError("Your saved recipes were not found", 404));
+
+
+
+  let result;
+   if( regUser.savedDishes.includes(dish._id)){result = dish;
+  }else{result = "This dish has not been saved"} 
+
+  //send a response
+  res.status(200).send({
+    status: "status",
+    data: result,
+  });
+});
 
 userController.unsaveDish = catchAsync(async (req, res, next) => {});
 
